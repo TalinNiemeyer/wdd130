23871f810bdba2b96c8c092d16b1a43f73b09490
talin niemeyer <talinniemeyer@gmail.com>
2023-03-16 01:56:40 -0600

    jjk

--- MTB/scripts/script.js
+++ MTB/scripts/script.js
@@ -1,23 +1,29 @@
-window.addEventListener("load", () => {
-    const loader1 = document.querySelector(".loader1");
+// window.addEventListener("load", () => {
+//     const loader1 = document.querySelector(".loader1");
   
-    loader1.classList.add("loader1--hidden");
+//     loader1.classList.add("loader1--hidden");
   
-    loader1.addEventListener("transitionend", () => {
-      document.body.removeChild(loader1);
-    });
+//     loader1.addEventListener("transitionend", () => {
+//       document.body.removeChild(loader1);
+//     });
 
-    const loader2 = document.querySelector(".loader2");
+//     const loader2 = document.querySelector(".loader2");
   
-    loader2.classList.add("loader2--hidden");
+//     loader2.classList.add("loader2--hidden");
   
-    loader2.addEventListener("transitionend", () => {
-      document.body.removeChild(loader2);
+//     loader2.addEventListener("transitionend", () => {
+//       document.body.removeChild(loader2);
 
-    document.getElementById('loader').style.display = 'none';
+//     document.getElementById('loader').style.display = 'none';
     
-    document.getElementById('hero').style.display = 'block';
-
-    });
-  });
+//     });
+//   });
   
+//   setTimeout(function() {
+//     var body = document.querySelector('body');
+//     body.classList.remove('hidden');
+//   }, 3000);
+
+
+// document.getElementById("menu").addEventListener("click", );  
+
